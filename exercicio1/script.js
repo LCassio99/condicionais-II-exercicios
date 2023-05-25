c1fabb667393e89357db88f1e02c6d6e043b80de
LCassio99 <lucianocassio66@gmail.com>
2023-05-25 19:43:36 -0300

    Exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,28 @@
+const numero = Number(prompt('Digite um número:'))
+
+  // if(numero % 2 === 0){
+  //   if (numero % 3 === 0){
+
+  //   console.log('Seu número é divisível por 2 ou por 3')
+    
+  // }else{
+
+  //   console.log('Seu número não é divisível por 2 nem por 3')
+
+  // }
+  // }
+
+  
+
+
+//---------------------------------------------------------------
+
+if (numero % 2 === 0 || numero % 3 === 0){
+
+  console.log('Seu número é divisível por 2 ou por 3')
+
+} else{
+
+  console.log('Seu número não é divisível por 2 nem por 3')
+
+}
